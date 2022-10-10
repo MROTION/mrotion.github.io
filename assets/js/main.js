141cfae0b4996f90f42518154ef5b90ce690ac92
ItzAnsh <anshbhasin3723@gmail.com>
2022-10-10 23:07:54 +0530

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,3 +272,8 @@ gsap.to(".impact", {
 	},
 	color: "#2c2d2d",
 });
+
+var size = [window.width, window.height]; //public variable.
+$(window).resize(function () {
+	window.resizeTo(size[0], size[1]);
+});
